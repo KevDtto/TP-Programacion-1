1c525d8814ec8138d4bc2111888a268716514291
KevDtto <104948198+KevDtto@users.noreply.github.com>
2022-05-12 20:31:18 -0300

    Add files via upload

---
+++ Juego.java
@@ -0,0 +1,133 @@
+package juego;
+
+import java.awt.Color;
+import java.awt.Image;
+
+import java.util.Random;
+
+import entorno.Entorno;
+import entorno.Herramientas;
+import entorno.InterfaceJuego;
+
+public class Juego extends InterfaceJuego {
+
+	// El objeto Entorno que controla el tiempo y otros
+	private Entorno entorno;
+	private Mikasa mikasa;
+	private Suero suero;
+	private Tiempo tiempo;
+	private Image imageFondo;
+//----------------------------
+	private int cantObs;
+	private Obstaculos[] obs;
+//----------------------------
+
+	public Juego() {
+		// Inicializa el objeto entorno
+		this.entorno = new Entorno(this, "Sakura Ikebana Delivery - Grupo N - Apellido1 - Apellido2 -Apellido3 - V0.01",
+				800, 600);
+		this.mikasa = new Mikasa(400, 300, 40, 40, 0);
+		this.suero = new Suero(0, 0, 0);
+		this.tiempo = new Tiempo(true, 0);
+		this.imageFondo = Herramientas.cargarImagen("FondoSeis.jpg");
+//-----------------------
+		this.cantObs = 5;
+		this.iniciarObs();
+//-----------------------
+		// Inicializar lo que haga falta para el juego
+		// ...
+
+		// Inicia el juego!
+		this.entorno.iniciar();
+	}
+
+	/**
+	 * Durante el juego, el método tick() será ejecutado en cada instante y por lo
+	 * tanto es el método más importante de esta clase. Aquí se debe actualizar
+	 * el estado interno del juego para simular el paso del tiempo (ver el enunciado
+	 * del TP para mayor detalle).
+	 */
+	public void tick() {
+		// Procesamiento de un instante de tiempo
+		// ...
+
+		if (tiempo.isInicia()) {
+			entorno.dibujarImagen(this.imageFondo, 400, 400, 0, 1.7);
+//			instrucciones
+			if (entorno.estaPresionada('w'))
+				mikasa.moverAdelante();
+
+			if (entorno.estaPresionada('a'))
+				mikasa.moverIzquierda();
+
+			if (entorno.estaPresionada('d'))
+				mikasa.moverDerecha();
+
+			if (suero.colision(mikasa.getX(), mikasa.getY(), suero.getX(), suero.getY(), suero.getRadio())) {
+				suero.setX(0);
+				suero.setY(0);
+				suero.setRadio(0);
+			}
+
+			if (tiempo.getContar() == 0.015 || (int) tiempo.getContar() == 19) {
+				suero.setX(Math.random() * 800);
+				suero.setY(Math.random() * 600);
+				suero.setRadio(30);
+			}
+
+//			dibujar
+			mikasa.dibujar(entorno);
+
+			if (((int) tiempo.getContar() > 10 && (int) tiempo.getContar() < 18)
+			 || ((int) tiempo.getContar() > 20 && (int) tiempo.getContar() < 28)) {
+				suero.dibujar(entorno);
+			}
+
+//			controla el fin del juego
+			if ((int) tiempo.getContar() > 40) {
+				this.tiempo = new Tiempo(false, 0);
+			}
+
+//			399 = 6,96 segundos reales
+			tiempo.setContar(0.015);
+
+//			imprime el tiempo
+			System.out.println(tiempo.getContar());
+//---------------------
+			for (Obstaculos o : this.obs) {
+				o.dibujar(entorno);
+			}
+//---------------------
+		} else {
+			entorno.cambiarFont("Arial", 32, Color.WHITE);
+			entorno.escribirTexto("GAME OVER", 300, 300);
+		}
+	}
+
+//--------------------------------------------------------------------
+	private void iniciarObs() {
+		Obstaculos[] lista = new Obstaculos[this.cantObs];
+		Random r = new Random();
+		int xPiso = 370;
+		int yPiso = 500;
+		int ancho = 80;
+		int alto = 90;
+		for (int i = 0; i < this.cantObs; i++) {
+			Obstaculos p = new Obstaculos(xPiso, yPiso, ancho, alto);
+			lista[i] = p;
+			yPiso = yPiso - 100;
+			xPiso = r.nextInt(600);
+			if (xPiso >= 350 && xPiso < 450) {
+				xPiso = r.nextInt(600);
+			}
+		}
+		this.obs = lista;
+	}
+
+//	----------------------------------------------------------------------------
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		Juego juego = new Juego();
+	}
+
+}
---
+++ Mikasa.java
@@ -0,0 +1,73 @@
+package juego;
+
+import java.awt.Color;
+
+import entorno.Entorno;
+
+public class Mikasa {
+	private double x;
+	private double y;
+	private double ancho;
+	private double alto;
+	private double angulo;
+
+	public Mikasa(double x, double y, double ancho, double alto, double angulo) {
+		this.x = x;
+		this.y = y;
+		this.ancho = ancho;
+		this.alto = alto;
+		this.angulo = angulo;
+	}
+
+	public void dibujar(Entorno e) {
+		e.dibujarRectangulo(this.x, this.y, this.ancho, this.alto, this.angulo, Color.ORANGE);
+	}
+
+	// movimiento
+	public void moverAdelante() {
+		if (this.x > 20 && ((this.x + this.ancho) < 820) && this.y < 590 && ((this.y - this.alto) > -30)) {
+			this.x += Math.cos(this.angulo) * 1.5;
+			this.y += Math.sin(this.angulo) * 1.5;
+		}
+		if (this.x <= 20) {
+			this.x = this.x + 0.5;
+		}
+		if ((this.x + this.ancho) >= 800) {
+			this.x = this.x - 0.5;
+		}
+		if (this.y >= 580) {
+			this.y = this.y - 0.5;
+		}
+		if ((this.y - this.alto) <= 0) {
+			this.y = this.y + 0.5;
+		}
+	}
+	
+	public void moverDerecha() {
+		this.angulo += 0.08;
+	}
+
+	public void moverIzquierda() {
+		this.angulo -= 0.08;
+	}
+
+//	getters and setters
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+
+
+}
